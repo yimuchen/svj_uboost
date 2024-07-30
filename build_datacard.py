4d4e9f81f8fdcb8f2cd677bff0e7c84378e1b720
Kevin Pedro <kpedro88@gmail.com>
2024-07-29 19:19:57 -0500

    add some useful printing features

--- build_datacard.py
+++ build_datacard.py
@@ -758,10 +758,44 @@ def get_yield(hist):
 def pct_diff(central,syst):
     return abs(1-syst/central)*100
 
+def printSigFigs(num,fig,maxdec):
+    # use scientific notation to get sig figs, then convert back to regular notation
+    sci = "{0:>.{1}e}".format(num,fig-1)
+    val = sci.split('e')[0]
+    expo = int(sci.split('e')[1])
+    pindex = val.find('.')
+    if expo==0:
+        result = val
+    elif expo>0:
+        if pindex==-1:
+            result = val
+        else:
+            # move decimal point
+            rval = val.replace('.','')
+            pindex2 = pindex+expo
+            # trailing zeroes
+            if pindex2 >= len(rval): result = rval+'0'*(pindex2-len(rval))
+            else: result = rval[0:pindex2]+'.'+rval[pindex2:]
+    else: # expo<0
+        rval = val if pindex==-1 else val.replace('.','')
+        # add leading zeroes
+        result = "0."+'0'*(abs(expo)-1)+rval
+    # recursive truncation
+    if '.' in result and len(result.split('.')[1])>maxdec:
+        if fig==1:
+            rnum = round(num,abs(expo)-1)
+            if rnum==0.0: return "0.0"
+            else: return printSigFigs(rnum,fig,maxdec)
+        else: return printSigFigs(num,fig-1,maxdec)
+    else:
+        if set(result.replace('.',''))==set([0]): return "0.0"
+        else: return result
+
 @scripter
 def systematics_table():
     change_bin_width()
     qtyrange = common.pull_arg('--qtyrange', metavar=("qty min max"), default=[], type=str, action='append', nargs=3).qtyrange
+    minimum = common.pull_arg('--minimum', type=float, default=0.01, help="minimum value to display, smaller values rounded to 0").minimum
     skimdir = common.pull_arg('skimdir', type=str).skimdir
     skims = expand_wildcards(skimdir)
 
@@ -839,10 +873,26 @@ def systematics_table():
         for syst in list(systs.keys())+["total"]:
             syst_effects["Overall"][syst] = (min([syst_effects[year][syst][0] for year in years]), max([syst_effects[year][syst][1] for year in years]))
 
+    # print settings
+    sigfig = 2
+    maxdec = int(abs(np.log10(minimum)))
+
     print(" & ".join(["Systematic"]+years)+r" \\")
     print(r"\hline")
     def print_syst_row(syst):
-        print(" & ".join([systs[syst]]+["{:.2f}--{:.2f}".format(*syst_effects[year][syst]) for year in years])+r" \\")
+        cols = [systs[syst]]
+        for year in years:
+            tmin = syst_effects[year][syst][0]
+            tmax = syst_effects[year][syst][1]
+            smin = printSigFigs(tmin,sigfig,maxdec)
+            smax = printSigFigs(tmax,sigfig,maxdec)
+            # don't bother to display a range if values are equal within precision
+            if abs(tmax-tmin)>minimum and smin != smax:
+                trange = smin+"--"+smax
+            else:
+                trange = smax
+            cols.append(trange)
+        print(" & ".join(cols)+r" \\")
     for syst in systs:
         print_syst_row(syst)
     print(r"\hline")
