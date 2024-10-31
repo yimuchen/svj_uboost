1700404fd601ebdb00e053ef3b9906854e37372a
Kevin Pedro <kpedro88@gmail.com>
2024-10-31 16:41:30 -0500

    allow skipping existing hadded skims

--- hadd_skims.py
+++ hadd_skims.py
@@ -36,6 +36,7 @@ if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument('--stageout', type=str, required=True)
+    parser.add_argument('--skip-existing', default=False, action="store_true")
     parser.add_argument('-n', '--nthreads', default=10, type=int)
     parser.add_argument('directories', nargs='+', type=str)
     args = parser.parse_args()
@@ -47,6 +48,9 @@ if __name__ == '__main__':
     fn_args = []
     for d in directories:
         outfile = args.stageout+'/'.join(d.split('/')[-2:])+'.npz'
+        if args.skip_existing and seutils.isfile(outfile):
+            svj.logger.info('    File %s exists, skipping', outfile)
+            continue
         fn_args.append((d, outfile))
 
     import multiprocessing as mp
