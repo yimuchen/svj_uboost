5d2f012369bf080764804bfc694d28a25d58c19e
Kevin Pedro <kpedro88@gmail.com>
2024-11-04 19:20:36 -0600

    store span optimization info, reduce minimum

--- build_datacard.py
+++ build_datacard.py
@@ -649,12 +649,14 @@ def smooth_shapes():
         if norm: hist = hist*(1./hyield)
 
         if do_opt>0 and (var==target or len(target)==0):
-            span_min = 0.1 # if span is too small, no points are included
+            span_min = 0.05 # if span is too small, no points are included
             spans = np.linspace(span_min,1.,do_opt,endpoint=False) # skip 1
             gcvs = np.array([do_loess(hist, span, do_gcv=True) for span in spans])
             span_val = spans[np.argmin(gcvs)]
             if debug: print('\n'.join(['{} {}'.format(span,gcv) for span,gcv in zip(spans,gcvs)]))
             print("Optimal span ({}): {}".format(var,span_val))
+            meta["span"] = span_val
+            meta["gcvs"] = list(gcvs)
 
         pred, conf = do_loess(hist,span=span_val)
 
@@ -677,6 +679,9 @@ def smooth_shapes():
                 hstat.vals = np.clip(ival,0,None)
                 hstat.errs = np.zeros_like(hstat.vals)
                 mths_new[f'stat_{iname}'] = hstat
+
+        # store updated metadata to avoid duplicating for stat variations
+        hsmooth.metadata = meta
 
     outdir = os.path.dirname(json_file).replace("hists","smooth").replace("merged","smooth")
     os.makedirs(outdir, exist_ok=True)
